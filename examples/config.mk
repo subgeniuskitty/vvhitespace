8bed3ccdaf4669b8563578a8832d333cd66aed24
Aaron Taylor <ataylor@subgeniuskitty.com>
2019-07-11 05:26:18 -0700

    Adding a 'Hello, World!' example using stdlib and a new build system using `cpp`.

---
+++ examples/config.mk
@@ -0,0 +1,11 @@
+# (c) 2019 Aaron Taylor <ataylor at subgeniuskitty dot com>
+# All rights reserved.
+
+####################################################################################################
+# Configuration
+
+VVS_COMPILER	= ../../vvc
+VVS_INTERPRETER	= ../../vvi
+
+CPP		= cpp
+CPP_FLAGS	= -I../../stdlib
